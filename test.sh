9b496c5bf7c69861065825cd6f6a39d010744473
Jack Maloney <jmmaloney4@gmail.com>
2017-10-12 14:50:45 -0500

    Delete test.sh

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for i in {1..50}; do
-	./.build/debug/Conductor Tests/Resources/rules.json Tests/Resources/europe.json > /dev/null
-	if [$? != 0]; then
-		"ERROR+++++++++++++++++++++++++++++++++++++++++"
-		 exit
-	fi
-done;
-
