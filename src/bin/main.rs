7b0ca18a7735ea9bf68b602accffaf378706f075
Jack Maloney <jmmaloney4@gmail.com>
2018-09-25 21:19:50 -0500

    more work

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,10 +1,2 @@
 mod conductor;
 
-fn main() {
-    println!("Hello, world!");
-
-    let london = conductor::City { name: String::from("London") };
-    let amsterdam = conductor::City { name: String::from("Amsterdam") };
-
-    let route = conductor::Route { ends: [&london, &amsterdam] };
-}
