de9e3c56dbd68c3f967e0445398d68681f5d7a0c
Jack Maloney <jmmaloney4@gmail.com>
2021-05-14 10:40:00 -0500

    Update dependency requirements

--- Package.swift
+++ Package.swift
@@ -10,12 +10,12 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/danger/swift.git", from: "3.10.1"),
-        .package(url: "https://github.com/kylef/Commander.git", from: "0.9.1"),
-        .package(url: "https://github.com/davecom/SwiftGraph.git", .branch("master")),
-        .package(url: "https://github.com/jmmaloney4/Squall.git", .branch("master")),
-        .package(url: "https://github.com/jpsim/Yams.git", from: "4.0.6"),
-        .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", .upToNextMajor(from: "1.9.0")),
+        .package(url: "https://github.com/danger/swift.git", .exact("3.10.1")),
+        .package(url: "https://github.com/kylef/Commander.git", .exact("0.9.1")),
+        .package(url: "https://github.com/davecom/SwiftGraph.git", .revision("59414e26a0d48aece7d842720f24bdebddfe9ae9")),
+        .package(url: "https://github.com/jmmaloney4/Squall.git", .revision("bbfdbe08df5e0364ddeaa3f092dba5e842f77d14")),
+        .package(url: "https://github.com/jpsim/Yams.git", .exact("4.0.6")),
+        .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", .exact("1.9.0")),
     ],
     targets: [
         .target(
